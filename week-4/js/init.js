1c79ffe4acc70f6210ad9d40fc5c2f8d17d3cbc0
Rajesh Gupta <rajesh_gupta@intuit.com>
2022-05-03 20:38:05 -0700

    huhhhh

--- week-4/js/init.js
+++ week-4/js/init.js
@@ -11,11 +11,11 @@ L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
 // create a function to add markers
 function addMarker(lat,lng,title,message){
     console.log(message)
-    L.marker([lat,lng]).addTo(map).bindPopup(`<h2>${title}</h2> <h3>${message}</h3>`)
+    L.marker([lat,lng]).addTo(map).bindPopup(`<h2>Please list any and all resources that have been useful for you in learning about or accessing PrEP. If possible, please include their address.-- ${title}</h2> <h3> In as much detail as you are comfortable with, please describe how each of the resources you listed above have helped you in learning about or accessing PrEP.-- ${message}</h3>`)
     return message
 }
 
-const dataUrl = "https://docs.google.com/spreadsheets/d/e/2PACX-1vS2WyfKTyZJ-_ja3GGrxoAXwranavyDGXYsxeFUO4nvHpCJrkKhChymXQqUEyhdGLnz9VN6BJv5tOjp/pub?gid=1560504149&single=true&output=csv"
+const dataUrl = "https://docs.google.com/spreadsheets/d/e/2PACX-1vSe3tKw51PzIC6dhUv5W8DEqg77o-usnZuTLBdgddFiD9rm3edq-Qa0pqJyOlSxnVtWEmR0YT--5Vmy/pub?gid=210560818&single=true&output=csv"
 
 function loadData(url){
     Papa.parse(dataUrl, {
@@ -29,7 +29,7 @@ function processData(results){
     console.log(results)
     results.data.forEach(data => {
         console.log(data)
-        addMarker(data.lat,data.lng,data['OpenEnded'],data['Is your English your first language?'])
+        addMarker(data.lat,data.lng,data["Please list any and all resources that have been useful for you in learning about or accessing PrEP. If possible, please include their address."],data["In as much detail as you are comfortable with, please describe how each of the resources you listed above have helped you in learning about or accessing PrEP."])
     })
 }
 // this is our function call to get the data
